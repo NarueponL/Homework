a112eee147baede2fcc4b54b08b4320333ce1eb0
Naruepon <78534947+NarueponL@users.noreply.github.com>
2021-03-28 02:16:07 +0700

    part3

--- Program.cs
+++ Program.cs
@@ -1,29 +1,96 @@
 ﻿using System;
 
-namespace part2
+namespace part3
 {
     class Program
     {
         static void Main(string[] args)
-        {                     
-            Console.Write("Input a : ");
-            int a = int.Parse(Console.ReadLine());
-            Console.Write("Input b : ");
-            int b = int.Parse(Console.ReadLine());
-            int x = a, y = b;
-
-            while (x != y)
+        {
+            //input
+            Console.Write("Input Number In Box A : ");
+            int numberBoxA = int.Parse(Console.ReadLine());
+            Console.Write("Input Number In Box B : ");
+            int numberBoxB = int.Parse(Console.ReadLine());
+            Console.Write("Input Number In Box C : ");
+            int numberBoxC = int.Parse(Console.ReadLine());
+            int turnCount = 1;
+            bool gameOverCheck = false;
+            while (gameOverCheck == false)
             {
-                if (x < y)
+                Console.WriteLine("\n----------------------------------------------------------------------\n");
+                Console.WriteLine("Turn : " + turnCount);
+                //status a
+                Console.WriteLine("Box A = {0}\tBox B = {1}\tBox C = {2}", numberBoxA, numberBoxB, numberBoxC);
+                
+                //player a
+                Console.WriteLine("\tTurn PlayerA");
+                Console.Write("Choose Box : ");
+                string chooseBox = Console.ReadLine();
+                Console.Write("How many Box Remove? : ");
+                int removeBox = int.Parse(Console.ReadLine());
+                switch (chooseBox)
                 {
-                    x = x + a;
+                    case "A":
+                        numberBoxA -= removeBox;
+                        break;
+                    case "B":
+                        numberBoxB -= removeBox;
+                        break;
+                    case "C":
+                        numberBoxC -= removeBox;
+                        break;
+                    default:
+                        break;
                 }
-                else
+                
+                //check player a
+                if(numberBoxA == 0 && numberBoxB == 0 && numberBoxC == 0)
                 {
-                    y = y + b;
-                }                
+                    gameOverCheck = true;
+                    Console.WriteLine("**********    Game Over Player B Win    **********");
+                    break;
+                }
+                turnCount++;
+                Console.WriteLine("\n----------------------------------------------------------------------\n");
+
+                //status b
+                Console.WriteLine("Turn : " + turnCount);
+                Console.WriteLine("Box A = {0}\tBox B = {1}\tBox C = {2}", numberBoxA, numberBoxB, numberBoxC);
+
+                //player b
+                Console.WriteLine("\tTurn PlayerB");
+                Console.Write("Choose Box : ");
+                chooseBox = Console.ReadLine();
+                Console.Write("How many Box Remove? : ");
+                removeBox = int.Parse(Console.ReadLine());
+                switch (chooseBox)
+                {
+                    case "A":
+                        numberBoxA -= removeBox;
+                        break;
+                    case "B":
+                        numberBoxB -= removeBox;
+                        break;
+                    case "C":
+                        numberBoxC -= removeBox;
+                        break;
+                    default:
+                        break;
+                }
+                
+                //check player b
+                if (numberBoxA == 0 && numberBoxB == 0 && numberBoxC == 0)
+                {
+                    gameOverCheck = true;
+                    Console.WriteLine("**********     Game Over Player A Win    **********");
+                    break;
+                }
+                turnCount++;
+
             }
-            Console.WriteLine("X = " + x);            
+
+
+
         }
     }
 }
