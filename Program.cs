ade5c5c48063e02c84efeb398f5b4d0d17f932a7
Naruepon <78534947+NarueponL@users.noreply.github.com>
2021-04-18 22:52:07 +0700

    Homework4-1

--- Program.cs
+++ Program.cs
@@ -1,29 +1,62 @@
 ﻿using System;
 
-namespace part2
+namespace HomeWork4_1
 {
     class Program
     {
-        static void Main(string[] args)
-        {                     
-            Console.Write("Input a : ");
-            int a = int.Parse(Console.ReadLine());
-            Console.Write("Input b : ");
-            int b = int.Parse(Console.ReadLine());
-            int x = a, y = b;
-
-            while (x != y)
+        static void pascalNum(int N)
+        {
+            for (int i = 0; i <= N; i++) 
             {
-                if (x < y)
+                for(int j = 0; j <= N; j++)
                 {
-                    x = x + a;
+                    if (j <= i) //ไม่คิด j ที่มากกว่า i
+                    {
+                        pascalFomula(i, j);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void pascalFomula(int n , int r)
+        {
+            
+            int a = factorial(n)/(factorial(r)*factorial(n-r));
+
+            Console.Write(" {0}",a);
+            
+        }
+
+        static int factorial(int n)
+        {
+            int x = 1;
+            for(int i=n;i>=1; i--)
+            {
+                x = x * i;
+            }
+            return x;
+        }
+
+        static void Main(string[] args)
+        {
+            int Input;
+            bool checkInput = false;
+            do
+            {
+                Input = int.Parse(Console.ReadLine());
+                if (Input < 0) 
+                {
+                    Console.WriteLine("Invalid Pascal’s triangle row number.");
                 }
                 else
                 {
-                    y = y + b;
-                }                
-            }
-            Console.WriteLine("X = " + x);            
+                    checkInput = true;
+                }
+                
+            } while (checkInput == false);
+            pascalNum(Input);
         }
+
     }
 }
