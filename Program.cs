60431d08b735f0fe46237117fbe22ace607ae7aa
Naruepon <78534947+NarueponL@users.noreply.github.com>
2021-04-24 15:35:32 +0700

    ข้อ5

--- Program.cs
+++ Program.cs
@@ -1,43 +1,85 @@
 ﻿using System;
 
-namespace Midterm_Exam_2
+namespace Midterm_Exam_3
 {
     class Program
-    {        
+    {
         static void Main(string[] args)
         {
-            double x, y, dx, dy, step;
-            double x1 = double.Parse(Console.ReadLine());
-            double x2 = double.Parse(Console.ReadLine());
-            double y1 = double.Parse(Console.ReadLine());
-            double y2 = double.Parse(Console.ReadLine());
-            dx = x2 - x1;
-            dy = y2 - y1;
-            if (Math.Abs(dx) >= Math.Abs(dy))
-            {
-                step = Math.Abs(dx);
-            }
-            else
-            {
-                step = Math.Abs(dy);
-            }
-            dx /= step;
-            dy /= step;
-            x = x1;
-            y = y1;
-            for(int i = 1; i <= step; i++)
-            {
-                Console.WriteLine("{0},{1}", x, y);
-                x += dx;
-                y += dy;
-            }
+            int breakfast = 5,weekend = 2,coffee=3;
+            int day = int.Parse(Console.ReadLine());
+            int time = int.Parse(Console.ReadLine());
+            bool checkOrder = false;
+            do
+            {                
+                Console.WriteLine("breakfast = {0}, weekend = {1}, coffee = {2}",breakfast,weekend,coffee);
+                string foodOrder = Console.ReadLine();
+                switch (foodOrder)
+                {
+                    case "Breakfast Set":
+                        //เช็คเวลา
+                        if (time > 11)
+                        {
+                            Console.WriteLine("Sorry your order is not available");
+                            break;
+                        }
+                        //เช็คจน.
+                        if (breakfast > 0)
+                        {
+                            breakfast--;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry your order is out of stock");
+                        }
+                        
+                        break;
 
+                    case "Weekend Set":
+                        //เช็ควัน
+                        if (day < 6) 
+                        {
+                            Console.WriteLine("Sorry your order is not available");
+                            break;
+                        }
+                        //เช็คจน.
+                        if (weekend > 0)
+                        {
+                            weekend--;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry your order is out of stock");
+                        }
+                        break;
 
+                    case "Coffee":
+                        //เช็คจน.
+                        if (coffee > 0)
+                        {
+                            coffee--;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry your order is out of stock");
+                        }
+                        break;
 
+                    case "End":
+                        checkOrder = true;
+                        break;
 
-
-
-
+                    default:
+                        Console.WriteLine("Please enter a valid menu");
+                        break;                    
+                }
+                //เช็คอาหารทั้งหมด
+                int checkFood = breakfast + weekend + coffee;
+                if (checkFood == 0)
+                {
+                    checkOrder = true;
+                }
+            } while (checkOrder == false); 
         }
     }
 }
