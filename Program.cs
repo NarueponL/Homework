d3edb0e39b8ad86ca1641837ad370cbf19ba6343
Naruepon <78534947+NarueponL@users.noreply.github.com>
2021-04-24 16:03:11 +0700

    ข้อ6

--- Program.cs
+++ Program.cs
@@ -1,85 +1,47 @@
 ﻿using System;
 
-namespace Midterm_Exam_3
+namespace Midterm_Exam_4
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            int breakfast = 5,weekend = 2,coffee=3;
-            int day = int.Parse(Console.ReadLine());
-            int time = int.Parse(Console.ReadLine());
-            bool checkOrder = false;
-            do
-            {                
-                Console.WriteLine("breakfast = {0}, weekend = {1}, coffee = {2}",breakfast,weekend,coffee);
-                string foodOrder = Console.ReadLine();
-                switch (foodOrder)
+
+            char write = char.Parse(Console.ReadLine());
+            int widh = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int step = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= step; i++)
+            {
+               
+                for (int j = 1; j <= widh*i; j++)
                 {
-                    case "Breakfast Set":
-                        //เช็คเวลา
-                        if (time > 11)
+                    for(int k = 1; k <= height; k++)
+                    {
+                        Console.Write(write);
+                        if (k == widh)
                         {
-                            Console.WriteLine("Sorry your order is not available");
-                            break;
+                            Console.WriteLine();
                         }
-                        //เช็คจน.
-                        if (breakfast > 0)
-                        {
-                            breakfast--;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Sorry your order is out of stock");
-                        }
-                        
-                        break;
-
-                    case "Weekend Set":
-                        //เช็ควัน
-                        if (day < 6) 
-                        {
-                            Console.WriteLine("Sorry your order is not available");
-                            break;
-                        }
-                        //เช็คจน.
-                        if (weekend > 0)
-                        {
-                            weekend--;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Sorry your order is out of stock");
-                        }
-                        break;
-
-                    case "Coffee":
-                        //เช็คจน.
-                        if (coffee > 0)
-                        {
-                            coffee--;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Sorry your order is out of stock");
-                        }
-                        break;
-
-                    case "End":
-                        checkOrder = true;
-                        break;
-
-                    default:
-                        Console.WriteLine("Please enter a valid menu");
-                        break;                    
+                    }
                 }
-                //เช็คอาหารทั้งหมด
-                int checkFood = breakfast + weekend + coffee;
-                if (checkFood == 0)
-                {
-                    checkOrder = true;
-                }
-            } while (checkOrder == false); 
+                Console.WriteLine();
+            }
+
+/*
+            */
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
