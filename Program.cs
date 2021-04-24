1497e01240e17bf57cf81409985010da0debe5e5
Naruepon <78534947+NarueponL@users.noreply.github.com>
2021-04-24 15:35:11 +0700

    ข้อ4

--- Program.cs
+++ Program.cs
@@ -1,78 +1,42 @@
 ﻿using System;
 
-namespace Midterm_Exam
+namespace Midterm_Exam_2
 {
     class Program
-    {
-        static double modefLength(out double fLength)
-        {            
-            bool checkfLength = false;
-            do
-            {
-                fLength = double.Parse(Console.ReadLine());
-                if (fLength < 0) 
-                {
-                    Console.WriteLine("Invalid fLength Please input again");
-                }
-                else
-                {
-                    checkfLength = true;                    
-                }
-            } while (checkfLength == false);
-            return fLength;
-        }
-        static double modeFov(out double fov)
-        {            
-            bool checkFov = false;
-            do
-            {
-                fov = double.Parse(Console.ReadLine());
-                if (fov < 0.1 && fov >6.28)
-                {
-                    Console.WriteLine("Invalid fLength Please input again");
-                }
-                else
-                {
-                    checkFov = true;                    
-                }
-            } while (checkFov == false);
-            return fov;
-        }
+    {        
         static void Main(string[] args)
         {
-            bool checkFlimBW = false;
-            double FLength, Fov, flimBackWidth;
-            string mode = Console.ReadLine();
-
-            do
+            double x, y, dx, dy, step;
+            double x1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            dx = x2 - x1;
+            dy = y2 - y1;
+            if (Math.Abs(dx) >= Math.Abs(dy))
             {
-                flimBackWidth = double.Parse(Console.ReadLine());
-                if (flimBackWidth<0)
-                {
-                    Console.WriteLine("Invalid filmBlackWidth Please input again");
-                }
-                else
-                {
-                    checkFlimBW = true;
-                }
-            } while (checkFlimBW == false);     
-
-            switch (mode)
-            {
-                case "fLength":
-                    FLength = modefLength(out FLength);
-                    Fov = 2 * Math.Atan(flimBackWidth/(2*FLength));
-                    Console.WriteLine("fLength = " + FLength);
-                    Console.WriteLine("fov = " + Fov);
-                    break;
-                case "fov":
-                    Fov = modeFov(out Fov);
-                    FLength = flimBackWidth / (2 * Math.Tan(Fov / 2));
-                    Console.WriteLine("fLength = " + FLength);
-                    Console.WriteLine("fov = " + Fov);
-                    break;
+                step = Math.Abs(dx);
             }
-            
+            else
+            {
+                step = Math.Abs(dy);
+            }
+            dx /= step;
+            dy /= step;
+            x = x1;
+            y = y1;
+            for(int i = 1; i <= step; i++)
+            {
+                Console.WriteLine("{0},{1}", x, y);
+                x += dx;
+                y += dy;
+            }
+
+
+
+
+
+
 
         }
     }
