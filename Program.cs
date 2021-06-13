04731e236f3cef46737c5dceeb40d4a454513806
Naruepon <78534947+NarueponL@users.noreply.github.com>
2021-06-13 17:25:51 +0700

    Final-6

--- Program.cs
+++ Program.cs
@@ -1,20 +1,12 @@
 ﻿using System;
 
-namespace Final_5
+namespace Final_6
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int numInventory = int.Parse(Console.ReadLine());
-            string[] itemName = new string[numInventory];
-            string[] itemType = new string[numInventory];
-
-            for (int i = 1; i < numInventory; i++)
-            {
-                itemName[i] = Console.ReadLine();
-                itemType[i] = Console.ReadLine();
-            }
+            Console.WriteLine("Hello World!");
         }
     }
 }
